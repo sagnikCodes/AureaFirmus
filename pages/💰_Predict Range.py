de9c416eafd08c2ef679a5832db6806181c6caee
sagnikCodes <114083986+sagnikCodes@users.noreply.github.com>
2023-07-06 13:13:21 +0530

    Update 💰_Predict Range.py

--- pages/💰_Predict Range.py
+++ pages/💰_Predict Range.py
@@ -17,14 +17,23 @@ st.set_page_config(
     page_title="📈Opening Price"
 )
 
+col1,col2,col3=st.columns(3)
+
 def load_lottieurl(url: str):
     r = requests.get(url)
     if r.status_code != 200:
         return None
     return r.json()
-    
-stocks=load_lottieurl("https://assets7.lottiefiles.com/packages/lf20_dwivte2j.json")
-st_lottie(stocks,width=1000,height=750)
+
+with col1:
+    stocks=load_lottieurl("https://assets7.lottiefiles.com/packages/lf20_bkozdh8w.json")
+    st_lottie(stocks,width=1000,height=750)
+with col2:
+    stocks=load_lottieurl("https://assets7.lottiefiles.com/packages/lf20_bkozdh8w.json")
+    st_lottie(stocks,width=1000,height=750)
+with col3:
+    stocks=load_lottieurl("https://assets7.lottiefiles.com/packages/lf20_dwivte2j.json")
+    st_lottie(stocks,width=1000,height=750)
 
 st.title('Predict Stock Price of any Company')
 st.write("Discover the anticipated range of tomorrow's opening price for any company with just a single click, providing you with valuable insights.")
