ac5380cb0f24d37452e6793376f1d91f7c9c529f
sagnikCodes <114083986+sagnikCodes@users.noreply.github.com>
2023-07-06 12:55:50 +0530

    Update 📈_Homepage.py

--- 📈_Homepage.py
+++ 📈_Homepage.py
@@ -1,4 +1,8 @@
 import streamlit as st
 
+st.set_page_config(
+    page_title="💸Homepage"
+)
+
 st.title('Welcome to AureaFirmus!')
 st.write("Presenting AureaFirmus - The Golden Path to Stock Prognostication.We are here to assist you every step of the way, providing invaluable insights and empowering you to make informed decisions. With our cutting-edge tools and comprehensive resources, we enable you to navigate the dynamic stock market landscape with confidence. Unleash the power of prediction as we unravel the opening range of stock prices, granting you a glimpse into the exciting realm of market trends. Additionally, our expertise extends to predicting closing prices, equipping you with a strategic advantage in optimizing your investment strategies. Let us be your trusted companion on this thrilling journey, as we empower you to unlock the full potential of your financial aspirations.")
