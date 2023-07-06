5d9f0880f6b3033bde79a9d8c240d04231b97cc6
sagnikCodes <114083986+sagnikCodes@users.noreply.github.com>
2023-07-06 13:01:30 +0530

    Update 📈_Homepage.py

--- 📈_Homepage.py
+++ 📈_Homepage.py
@@ -16,7 +16,7 @@ col1,col2=st.columns(2)
 
 with col1:
     st.title('Welcome to AureaFirmus!')
-    st.write("Presenting AureaFirmus - The Golden Path to Stock Prognostication.We are here to assist you every step of the way, providing invaluable insights and empowering you to make informed decisions. With our cutting-edge tools and comprehensive resources, we enable you to navigate the dynamic stock market landscape with confidence. Unleash the power of prediction as we unravel the opening range of stock prices, granting you a glimpse into the exciting realm of market trends. Additionally, our expertise extends to predicting closing prices, equipping you with a strategic advantage in optimizing your investment strategies. Let us be your trusted companion on this thrilling journey, as we empower you to unlock the full potential of your financial aspirations.")
+    st.write("Presenting AureaFirmus - The Golden Path to Stock Prognostication.We are here to assist you every step of the way, providing invaluable insights and empowering you to make informed decisions. Additionally, our expertise extends to predicting closing prices, equipping you with a strategic advantage in optimizing your investment strategies. Let us be your trusted companion on this thrilling journey, as we empower you to unlock the full potential of your financial aspirations.")
 with col2:
     stocks=load_lottieurl("https://assets7.lottiefiles.com/packages/lf20_kuhijlvx.json")
-    st_lottie(stocks,width=400,height=200)
+    st_lottie(stocks,width=500,height=350)
